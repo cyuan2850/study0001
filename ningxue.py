7081147b0dde608a85b94f1b158d5e3a6577b9a4
peng <cyuan2850@gmail.com>
2020-01-17 15:16:52 +0800

    bog fix

--- ningxue.py
+++ ningxue.py
@@ -491,12 +491,12 @@ def restart_program():
 def bot(op):
     try:
         print(type(op))
-        print("\n---\n"
+        print("\n---\n")
         for data in op:
-            print("\n---\n"
+            print("\n---\n")
             print(type(data))
             print(data)
-        print("\n---\n"
+        print("\n---\n")
             
         if op.type == 0:
             return
